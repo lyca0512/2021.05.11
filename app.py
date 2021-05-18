778796a2a795b0c6f715f09616de90b0f1d312f2
lycan0512 <lycan0512@naver.com>
2021-05-18 16:52:49 +0900

    2021.05.18(GET POST)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,11 +16,22 @@ def index():
 
 @app.route('/hello')
 def hello():
-    return 'Hello world'
+    return render_template("main.html")
 
 @app.route('/naver')
 def naver():
-    return 'NAVER 입니다'
+    return render_template("naver.html")
+
+@app.route('/gonaver', methods=['GET', 'POST'])
+def gonaver():
+    if request.method == 'GET':
+     return "데이터를 받아주는 페이지"
+    else:
+     #여기 POST로 들어오는 데이터를 받아보자
+     search =  request.form['fname']
+     print("전달된값:",search)
+     return "당신이 검색한 키워드(POST)<br>{}입니다".format(search)
+     
 
 
 if __name__ == '__main__':
