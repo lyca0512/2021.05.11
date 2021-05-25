88065fee66bca5656ac6c5928402cb1e594886e1
lycan0512 <lycan0512@nate.com>
2021-05-25 17:17:03 +0900

    2021.05.25

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def index():
 <p><a href="http://127.0.0.1:5000/hello">헬로우 페이지</a></p>
 <p><a href="http://127.0.0.1:5000/naver">네이버 페이지</a></p>
 <p><a href="http://127.0.0.1:5000/singin">가입 페이지</a></p>
+<p><a href="http://127.0.0.1:5000/login">로그인 페이지</a></p>
 </body>
 </html>
 '''
@@ -47,6 +48,24 @@ def action_page():
      password =  request.form['pwd']
      print("전달된값:",email, password)
      return "회원가입 데이터(POST)"
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+     
+    else:
+     #여기 POST로 들어오는 데이터를 받아보자
+      email =  request.form['email']
+      password =  request.form['pwd']
+      print("전달된값:",email, password)
+      if email ==  'a@a.com' and password == '1234':
+       return("로그인 성공")
+      else:
+       print("아이디 패스워드 확인")
+       return "로그인 데이터(POST)"
 
 
 if __name__ == '__main__':
